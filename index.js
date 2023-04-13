d748d253533882ea181cf45d04ccc3690cf39258
Vipin <64310652+vipinbaghel@users.noreply.github.com>
2023-04-13 18:37:57 +0530

    updated

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ let input = document.getElementById("inp3");
 let output = getElementById("inp4")
 input = (input.value);
 
-var sum = 0;
 
-for(let i=0; i<10; i++){
-    sum = sum + Number(input.charAt(i));
-}
 
-var ans = ""
-for(let i=1; i<=sum; i++)
-{
-    if(i%20 == 0){
-        ans += "FizzBuzz ";
-    } else if(i % 4 == 0){
-        ans += "Fizz ";
-    } else if(i%5 == 0){
-        ans += "Buzz";
-    } else {
-        ans += i;
-        ans += " ";
+function submitForm(e) {
+    e.preventDefault();
+    var sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum = sum + Number(input.charAt(i));
     }
+
+    var ans = ""
+    for (let i = 1; i <= sum; i++) {
+        if (i % 20 == 0) {
+            ans += "FizzBuzz ";
+        } else if (i % 4 == 0) {
+            ans += "Fizz ";
+        } else if (i % 5 == 0) {
+            ans += "Buzz";
+        } else {
+            ans += i;
+            ans += " ";
+        }
+    }
+
+    output.innetText = ans;
+
+
 }
-
-output.innetText = ans;
-
